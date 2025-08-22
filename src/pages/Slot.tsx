fbc0fbf306ccea050856fe4d7983697eb02442c8
Jan Zemlicka <jzemlicka@trask.cz>
2025-08-22 11:11:28 +0200

    Initial commit

--- src/pages/Slot.tsx
+++ src/pages/Slot.tsx
@@ -5,21 +5,23 @@ interface SlotProps {
     isBooked: boolean;
     isBreak: boolean;
     onClick: () => void;
+    hasUserBooked: boolean;
 }
 
-const Slot: React.FC<SlotProps> = ({ time, isBooked, isBreak, onClick }) => {
+const Slot: React.FC<SlotProps> = ({ time, isBooked, isBreak, onClick, hasUserBooked }) => {
     let className = 'btn w-100 btn-slot';
     if (isBreak) className += ' btn-break';
+    else if (hasUserBooked) className += ' btn-user-booked';
     else if (isBooked) className += ' btn-booked';
     else className += ' btn-available';
 
     return (
         <button
             className={className}
-            onClick={isBreak ? undefined : onClick}
-            disabled={isBreak}
+            onClick={isBreak || isBooked ? undefined : onClick}
+            disabled={isBreak || isBooked}
         >
-            {time}
+            {isBooked ? 'Booked' : time}
         </button>
     );
 };
